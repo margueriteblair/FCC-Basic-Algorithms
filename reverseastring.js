10dee9b2ff0701c73590058d2396ce3a31adafe4
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-09 13:56:06 -0400

    made congruent function to practice

--- reverseastring.js
+++ reverseastring.js
@@ -7,3 +7,23 @@ function reverseString(str) {
     
 }
 reverseString("butts");
+
+
+
+
+
+
+
+
+
+
+
+
+function reverseString2(str){
+    let reverseStr = "";
+    for (i = str.length-1; i >= 0; i--) {
+        reverseStr = str[i];
+        console.log(reverseStr)
+    }
+}
+reverseString2("CareerDevs");
